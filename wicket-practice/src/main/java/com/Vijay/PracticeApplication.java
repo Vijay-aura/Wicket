f3c29dfbf20d5c242bcdd9451688fb5af6989e6e
Vijay Kumar <vijay.k@auracloud.com>
2024-03-06 18:53:09 +0530

    Submitted Form using Ajax link

--- wicket-practice/src/main/java/com/Vijay/PracticeApplication.java
+++ wicket-practice/src/main/java/com/Vijay/PracticeApplication.java
@@ -2,16 +2,19 @@ package com.Vijay;
 
 import org.apache.wicket.Page;
 
+
 import org.apache.wicket.protocol.http.WebApplication;
 
-import com.form.InputFieldsPage;
+import com.AjaxSubmitForm.MainPage;
+
+
 
 public class PracticeApplication extends WebApplication {
 
 	@Override
 	public Class<? extends Page> getHomePage() {
 		// TODO Auto-gener+ated method stub
-        return  InputFieldsPage.class;
+        return  MainPage.class;
 	}
 	 
 }
